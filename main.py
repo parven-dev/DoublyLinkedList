21be5ca3b1dd5ad036d31425e787b8c6817d361d
parven-dev <ryan.cu1200@gmail.com>
2024-11-26 15:45:55 +0530

    added pop method

--- main.py
+++ main.py
@@ -13,6 +13,36 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1
 
+
+    def append(self, value):
+        new_node = Node(value)
+        temp = self.head
+        if temp is None:
+            self.head = new_node
+            self.tail = new_node
+            self.length +=1
+            
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+            
+            
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        self.tail = self.tail.prev
+        self.tail.next = None
+        temp.prev = None
+        
+        self.length -= 1
+
+        print("poped item", temp.value)
+        
+        
+        
+        
     
             
     def display(self):
@@ -25,4 +55,9 @@ class DoublyLinkedList:
 
 
 d1 = DoublyLinkedList(4)
+d1.append(49)
+d1.append(50)
+d1.append(509)
+
+d1.pop()
 d1.display()
